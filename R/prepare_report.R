72093ba4ba224bbfc48a1ca702ea16f03e2468f9
Mr. Cryptoltruist <93457844+cryptoltruist@users.noreply.github.com>
2023-02-17 20:09:46 -0500

    fix some report arguments

--- R/prepare_report.R
+++ R/prepare_report.R
@@ -2,6 +2,8 @@
 #'
 #' @description Prepare all required information for a full crypto tax report.
 #' @param formatted.ACB The `formatted.ACB` object.
+#' @param tax.year The tax year desired.
+#' @param local.timezone Which time zone to use for the date of the report.
 #' @param list.prices A `list.prices` object from which to fetch coin prices.
 #' @return A list, containing the following objects: report.overview, 
 #' report.summary, proceeds, sup.losses, table.revenues, tax.box, 
@@ -14,16 +16,19 @@
 #' x <- prepare_report(formatted.ACB, list.prices)
 #' x$proceeds
 
-prepare_report <- function(formatted.ACB, list.prices = NULL) {
+prepare_report <- function(formatted.ACB, 
+                           tax.year = "all", 
+                           local.timezone = Sys.timezone(), 
+                           list.prices = NULL) {
   report.overview <- report_overview(
-    formatted.ACB, today.data = TRUE, tax.year = "all", 
-    local.timezone = "America/Toronto", list.prices = list.prices)
+    formatted.ACB, today.data = TRUE, tax.year = tax.year, 
+    local.timezone = local.timezone, list.prices = list.prices)
   report.summary <- report_summary(
-    formatted.ACB, today.data = TRUE, tax.year = "all", 
-    local.timezone = "America/Toronto", list.prices = list.prices)
-  proceeds <- get_proceeds(formatted.ACB, tax.year = 2021)
-  sup.losses <- get_sup_losses(formatted.ACB, 2021)
-  table.revenues <- report_revenues(formatted.ACB, tax.year = "all")
+    formatted.ACB, today.data = TRUE, tax.year = tax.year, 
+    local.timezone = local.timezone, list.prices = list.prices)
+  proceeds <- get_proceeds(formatted.ACB, tax.year = tax.year)
+  sup.losses <- get_sup_losses(formatted.ACB, tax.year)
+  table.revenues <- report_revenues(formatted.ACB, tax.year = tax.year)
   tax.box <- tax_box(report.summary, sup.losses, table.revenues, proceeds)
   pie_exchange <- crypto_pie(table.revenues)
   pie_revenue <- crypto_pie(table.revenues, by = "revenue.type")
